b7295a3aab7c2a22abcbc469c0aa5b37b510721f
Nic0Nic0Nii <sigonasr2@yahoo.com>
2022-05-23 18:02:14 +0000

    Updated SigScripts Co-authored-by: r3cp3ct <45179536+r3cp3ct@users.noreply.github.com>

--- Java/scripts/commit.sh
+++ Java/scripts/commit.sh
@@ -1,14 +1,6 @@
 #Adds a commit message and pushes project to github repository.
-#
-
-source utils/search.sh
-
-find . -type f -name md5 -delete
-
-#Generate a new hash for every sub-directory, which may require an update.
-FILES=$(cat utils/.updateDirectories) 
+#Java
 COMMIT_MESSAGE="$*"
-FIRST_LINE=true
 while IFS= read -r line
 do
 if [ "$FIRST_LINE" = true ]; then
@@ -21,10 +13,6 @@ else
 Co-authored-by: $line"
 fi
 done < utils/.coauthors
-for f in $FILES
-do 
-    search $f
-done
 git add -u
 git add *
 git commit -m "$COMMIT_MESSAGE"
