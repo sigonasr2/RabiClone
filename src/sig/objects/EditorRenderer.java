73882feb2ccc571cccbf60228908f4cb701d7158
Nic0Nic0Nii <sigonasr2@yahoo.com>
2022-06-02 19:43:51 +0000

    Change bottom display to show current map tile instead
    Co-authored-by: r3cp3ct <45179536+r3cp3ct@users.noreply.github.com>
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- src/sig/objects/EditorRenderer.java
+++ src/sig/objects/EditorRenderer.java
@@ -103,7 +103,7 @@ public class EditorRenderer extends LevelRenderer{
             double tileY = y*Tile.TILE_HEIGHT-this.getY();
             DrawTransparentTile(tileX,tileY,selectedTile,Alpha.ALPHA160);
             Draw_Text(tileX+2,tileY-Font.PROFONT_12.getGlyphHeight()-2,new StringBuilder(selectedTile.toString()),Font.PROFONT_12);
-            Draw_Text_Ext(tileX+2,tileY+Tile.TILE_HEIGHT+2,new StringBuilder(selectedTile.toString()),Font.PROFONT_12,Alpha.ALPHA0,PaletteColor.CRIMSON);
+            Draw_Text_Ext(tileX+2,tileY+Tile.TILE_HEIGHT+2,new StringBuilder(RabiClone.CURRENT_MAP.getTile(x,y).toString()),Font.PROFONT_12,Alpha.ALPHA0,PaletteColor.CRIMSON);
         }
     }    
 }
