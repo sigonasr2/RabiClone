09ea7a4d608b654492be4980092b785c8634390b
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-06-14 18:27:44 +0000

    linux uses exits instead of returns
    Co-authored-by: r3cp3ct <45179536+r3cp3ct@users.noreply.github.com>
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- runGame.sh
+++ runGame.sh
@@ -1,14 +1,14 @@
 cd bin
 if java -Djava.library.path="../lib" -Xms2G -Xmx2G -XX:+UseZGC -XX:+PrintCommandLineFlags -jar RabiClone.jar; then
     cd ..
-    return
+    exit
 fi
 if java -Djava.library.path="../lib" -Xms2G -Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+PrintCommandLineFlags -jar RabiClone.jar; then
     cd ..
-    return
+    exit
 fi
 if java -Djava.library.path="../lib" -Xms2G -Xmx2G -XX:+PrintCommandLineFlags -jar RabiClone.jar; then
     cd ..
-    return
+    exit
 fi
 cd ..
