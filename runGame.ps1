0845de0a7c187644bcd3e2f6c14d4a3e998c6054
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-06-14 17:21:46 +0000

    run scripts updated with java optimal options
    Co-authored-by: r3cp3ct <45179536+r3cp3ct@users.noreply.github.com>
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- runGame.ps1
+++ runGame.ps1
@@ -1,3 +1,13 @@
 cd bin
+java "-Djava.library.path=../lib" -Xms2G -Xmx2G -XX:+UseZGC -XX:+PrintCommandLineFlags -jar RabiClone.jar
+if ($? -eq $True) {
+    cd ..
+    return
+}
+java "-Djava.library.path=../lib" -Xms2G -Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+PrintCommandLineFlags -jar RabiClone.jar
+if ($? -eq $True) {
+    cd ..
+    return 
+}
 java "-Djava.library.path=../lib" -Xms2G -Xmx2G -XX:+PrintCommandLineFlags -jar RabiClone.jar
 cd ..
--- runGame.sh
+++ runGame.sh
@@ -1,3 +1,14 @@
 cd bin
-java -Djava.library.path="../lib" -Xms2G -Xmx2G -XX:+PrintCommandLineFlags -jar RabiClone.jar
+if java -Djava.library.path="../lib" -Xms2G -Xmx2G -XX:+UseZGC -XX:+PrintCommandLineFlags -jar RabiClone.jar; then
+    cd ..
+    return
+fi
+if java -Djava.library.path="../lib" -Xms2G -Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+PrintCommandLineFlags -jar RabiClone.jar; then
+    cd ..
+    return
+fi
+if java -Djava.library.path="../lib" -Xms2G -Xmx2G -XX:+PrintCommandLineFlags -jar RabiClone.jar; then
+    cd ..
+    return
+fi
 cd ..
